5dff2ff1e8416e47b40b58509f3c5d39e88d2709
Kailey1 <chen.kailey1@gmail.com>
2021-01-10 00:42:02 -0500

    api data attempt

--- app/src/main/java/cooldudes/stalkmarket/ui/fragment/GardenFragment.java
+++ app/src/main/java/cooldudes/stalkmarket/ui/fragment/GardenFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ScheduledExecutorService;
 
 import cooldudes.stalkmarket.R;
 import cooldudes.stalkmarket.ui.activity.MainActivity;
@@ -25,14 +29,16 @@ public class GardenFragment extends Fragment {
 
     // Firebase
     DatabaseReference fireRef = FirebaseDatabase.getInstance().getReference();
-
+    public static TextView data;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_garden, null);
 
         main = (MainActivity) getActivity();
-
+        data = (TextView) view.findViewById(R.id.stocks);
+        fetchData process = new fetchData();
+        process.execute();
         return view;
     }
 
